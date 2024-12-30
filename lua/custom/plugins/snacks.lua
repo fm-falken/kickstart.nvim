0ce34083deb7c46947d76a213f028c1be7b24889
Ilia Druta <ilia@druta.me>
2024-12-31 01:49:02 +0200

    added default snacks config

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,76 +1,22 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    dashboard = 
-      ---@class snacks.dashboard.Config
-      ---@field enabled? boolean
-      ---@field sections snacks.dashboard.Section
-      ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
-{
-  width = 60,
-  row = nil, -- dashboard position. nil for center
-  col = nil, -- dashboard position. nil for center
-  pane_gap = 4, -- empty columns between vertical panes
-  autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
-  -- These settings are used by some built-in sections
-  preset = {
-    -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
-    ---@type fun(cmd:string, opts:table)|nil
-    pick = nil,
-    -- Used by the `keys` section to show keymaps.
-    -- Set your custom keymaps here.
-    -- When using a function, the `items` argument are the default keymaps.
-    ---@type snacks.dashboard.Item[]
-    keys = {
-      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-      { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      indent = { enabled = false },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = false },
+      words = { enabled = true },
     },
-    -- Used by the `header` section
-    header = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
   },
-  -- item field formatters
-  formats = {
-    icon = function(item)
-      if item.file and item.icon == "file" or item.icon == "directory" then
-        return M.icon(item.file, item.icon)
-      end
-      return { item.icon, width = 2, hl = "icon" }
-    end,
-    footer = { "%s", align = "center" },
-    header = { "%s", align = "center" },
-    file = function(item, ctx)
-      local fname = vim.fn.fnamemodify(item.file, ":~")
-      fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
-      if #fname > ctx.width then
-        local dir = vim.fn.fnamemodify(fname, ":h")
-        local file = vim.fn.fnamemodify(fname, ":t")
-        if dir and file then
-          file = file:sub(-(ctx.width - #dir - 2))
-          fname = dir .. "/…" .. file
-        end
-      end
-      local dir, file = fname:match("^(.*)/(.+)$")
-      return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
-    end,
-  },
-  sections = {
-    { section = "header" },
-    { section = "keys", gap = 1, padding = 1 },
-    { section = "startup" },
-  },
-}
-  }
 }
